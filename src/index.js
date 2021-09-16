93eae33322d930016192d0ba67f311d1d07edb13
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-09-16 13:15:38 +0530

    style: footer updated

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
-const noteStore = [2000, 500, 200, 100, 20, 10, 5, 1];
+const billAmount = document.querySelector('#bill-amount')
+const cashGiven = document.querySelector('#cash-given')
+const checkButton = document.querySelector('#check-button')
+const errorMessage = document.querySelector('#error-message')
+const noOfNotes = document.querySelectorAll('.no-of-notes')
+const noteStore = [2000, 500, 200, 100, 20, 10, 5, 1]
 
-checkButton.addEventListener("click", function validateBillAmount() {
-  hideMessage();
+checkButton.addEventListener('click', function validateBillAmount() {
+  hideMessage()
   if (billAmount.value > 0) {
-    validateCashAmount();
+    validateCashAmount()
   } else {
-    showMessage("Bill Amount must be greater than 0", "orange");
+    showMessage('Bill Amount must be greater than 0', 'orange')
   }
-});
+})
 
 function validateCashAmount() {
-  if (cashGiven.value >= billAmount.value) {
-    const cashToReturn = cashGiven.value - billAmount.value;
-    calculateNotes(cashToReturn);
+  if (billAmount.value <= 0) {
+    showMessage('Invalid cash amount', 'red')
+  } else if (cashGiven.value <= 0) {
+    showMessage('Cash Given cannot be negative or 0', 'red')
+  } else if (cashGiven.value - billAmount.value === 0) {
+    showMessage('No need to return change😊', 'green')
+  } else if (cashGiven.value - billAmount.value > 0) {
+    const cashToReturn = cashGiven.value - billAmount.value
+    calculateNotes(cashToReturn)
   } else {
-    showMessage("Invalid cash amount", "red");
+    showMessage('Invalid cash amount', 'red')
   }
 }
 
 function calculateNotes(cashToReturn) {
   for (let i = 0; i < noteStore.length; i++) {
-    const numberOfNotes = Math.trunc(cashToReturn / noteStore[i]);
-    cashToReturn = cashToReturn % noteStore[i];
-    noOfNotes[i].innerText = numberOfNotes;
+    const numberOfNotes = Math.trunc(cashToReturn / noteStore[i])
+    cashToReturn = cashToReturn % noteStore[i]
+    noOfNotes[i].innerText = numberOfNotes
   }
-  showMessage("Trust us and give the following change😎", "green");
+  showMessage('Trust us and give the following change😎', 'green')
 }
 
 function hideMessage() {
-  errorMessage.style.display = "none";
+  errorMessage.style.display = 'none'
 }
 
 function showMessage(msg, color) {
-  errorMessage.style.display = "block";
-  errorMessage.style.background = color;
-  errorMessage.innerText = msg;
+  errorMessage.style.display = 'block'
+  errorMessage.style.background = color
+  errorMessage.innerText = msg
 }
